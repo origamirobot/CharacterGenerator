0b9c17fecf66b32735759376cf5bd0805cfb7cee
Chris Lees <chris.t.lees@gmail.com>
2015-07-08 14:33:08 -0400

    Initial commit of the character generator

---
+++ CharGen.Data/Models/Character.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharGen.Data.Models
+{
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Character
+	{
+
+		#region PUBLIC ACCESSORS
+
+
+		/// <summary>
+		/// Gets or sets the strength.
+		/// </summary>
+		public int Strength { get; set; }
+
+		/// <summary>
+		/// Gets or sets the dexterity.
+		/// </summary>
+		public int Dexterity { get; set; }
+
+		/// <summary>
+		/// Gets or sets the constitution.
+		/// </summary>
+		public int Constitution { get; set; }
+
+		/// <summary>
+		/// Gets or sets the intelligence.
+		/// </summary>
+		public int Intelligence { get; set; }
+
+		/// <summary>
+		/// Gets or sets the wisdom.
+		/// </summary>
+		public int Wisdom { get; set; }
+
+		/// <summary>
+		/// Gets or sets the charisma.
+		/// </summary>
+		public int Charisma { get; set; }
+
+
+		#endregion PUBLIC ACCESSORS
+
+
+
+	}
+
+}
---
+++ CharGen.Data/Models/Race.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharGen.Data.Models
+{
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Race
+	{
+
+		/// <summary>
+		/// Gets or sets the identifier.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		public String Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the description.
+		/// </summary>
+		public String Description { get; set; }
+
+		/// <summary>
+		/// Gets or sets the examples.
+		/// </summary>
+		public String Examples { get; set; }
+
+	}
+
+}
